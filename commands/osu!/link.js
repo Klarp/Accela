7b3d00ab69ceb803aa2e8b2153b519076b92181c
Brody Jagoe <nascarbrod@gmail.com>
2020-08-16 17:54:29 -0600

    Updae link.js
    - Re-added use of special characters

--- commands/osu!/link.js
+++ commands/osu!/link.js
@@ -16,18 +16,18 @@ module.exports = {
 				user_id: message.author.id,
 				user_osu: user,
 			});
-			return message.channel.send(`Linked ${message.author} to ${args.join(' ').replace(/[^\w\s]/gi, '')}`);
+			return message.channel.send(`Linked ${message.author} to ${args.join(' ')}`);
 		} catch(e) {
 			if (e.name === 'SequelizeUniqueConstraintError') {
 				try {
 					const upUser = await Users.update({
-						user_osu: args.join(' ').replace(/[^\w\s]/gi, ''),
+						user_osu: args.join(' '),
 					},
 					{
 						where: { user_id: message.author.id },
 					});
 					if (upUser > 0) {
-						return message.reply(`Updated link to ${args.join(' ').replace(/[^\w\s]/gi, '')}`);
+						return message.reply(`Updated link to ${args.join(' ')}`);
 					}
 				} catch(err) {
 					console.error(err);
