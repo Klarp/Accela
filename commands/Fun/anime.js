2475adca68292e48c675de22047cc57f0a7efb8e
Brody Jagoe <nascarbrod@gmail.com>
2020-08-23 12:02:42 -0600

    API Update
    - Changed api from MAL to AniList

--- commands/Fun/anime.js
+++ commands/Fun/anime.js
@@ -1,5 +1,6 @@
-const jikan = require('jikanjs');
+const anilist_node = require('anilist-node');
 const Discord = require('discord.js');
+const aniList = new anilist_node();
 
 module.exports = {
 	name: 'anime',
@@ -8,70 +9,52 @@ module.exports = {
 	module: 'Fun',
 	cooldown: 5,
 	args: true,
-	usage: '[name] or [name] -2',
+	usage: '[name]',
 	execute(message, args) {
 		const anime = args.join(' ');
-		console.log(anime.length);
-		if (anime.length < 3) return message.reply('The given query must be of minimum 3 letters');
+		aniList.search('anime', anime).then(res => {
+			aniList.media.anime(res.media[0].id).then(aniRes => {
+				function truncate(str, n) {
+					return (str.length > n) ? str.substr(0, n - 1) + '...' : str;
+				}
 
-		jikan.search('anime', anime).then((res) => {
-			const aniResult = res.results[0];
+				const status = aniRes.status || 'Unknown';
+				const type = aniRes.format || 'Unknown';
+				const episodes = aniRes.episodes || 'Unknown';
+				const studio = aniRes.studios[0].name || 'Unknown';
+				const avgScore = aniRes.averageScore || '0';
+				const genres = aniRes.genres.join(' | ');
+				let startDate = `${aniRes.startDate.year}-${aniRes.startDate.month}-${aniRes.startDate.day}`;
+				let endDate = `${aniRes.endDate.year}-${aniRes.endDate.month}-${aniRes.endDate.day}`;
+				let longDesc = aniRes.description.replace(/<\/?[^>]+(>|$)/g, '').replace(/&lsquo;/g, '').replace(/\n/g, '');
 
-			if (aniResult.rated === 'Rx') return message.reply('Requested anime was NSFW and has been denied');
-			if (!aniResult.rated) return message.reply('An error has occured and we cannot determine if the anime is NSFW. Please try again later.');
+				if (aniRes.startDate.year === null) {
+					startDate = 'Unknown';
+				}
 
-			const sd = aniResult.start_date.split('T')[0];
-			const ed = aniResult.end_date.split('T')[0];
+				if (aniRes.endDate.year === null) {
+					endDate = 'Unknown';
+				}
 
-			if (anime.includes('-')) {
-				const numArg = anime.split('-');
-				const num = numArg[1];
-				if (isNaN(num)) return;
-				console.log(num);
-				const newResult = res.results[num];
-
-				if (newResult.rated === 'Rx') return message.reply('Requested anime was NSFW and has been denied');
-				if (!aniResult.rated) return message.reply('An error has occured and we cannot determine if the anime is NSFW. Please try again later.');
-
-				const newSd = newResult.start_date.split('T')[0];
-				const newEd = newResult.end_date.split('T')[0];
+				longDesc = truncate(longDesc, 300);
 
 				const aniEmbed = new Discord.MessageEmbed()
-					.setAuthor('MAL Search', 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png')
+					.setAuthor('AniList', 'https://anilist.co/img/icons/android-chrome-512x512.png')
 					.setColor('BLUE')
-					.setTitle(newResult.title)
-					.setThumbnail(newResult.image_url, 500, 500)
-					.setURL(newResult.url)
-					.setDescription(`**Airing** ${newResult.airing} 
-**Episodes** ${newResult.episodes || 'Unknown'} **Type** ${newResult.type}
-**Score** ${newResult.score} **Rating** ${newResult.rated}
-
-**Start Date** ${newSd}
-**End Date** ${newEd}
-`)
-					.setFooter('Click on the title for full synopsis');
-				return message.channel.send(aniEmbed);
-			}
-
-			const aniEmbed = new Discord.MessageEmbed()
-				.setAuthor('MAL Search', 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png')
-				.setColor('BLUE')
-				.setTitle(aniResult.title)
-				.setThumbnail(aniResult.image_url, 500, 500)
-				.setURL(aniResult.url)
-				.setDescription(`**Airing** ${aniResult.airing} 
-**Episodes** ${aniResult.episodes || 'Unknown'} **Type** ${aniResult.type}
-**Score** ${aniResult.score} **Rating** ${aniResult.rated}
-
-**Start Date** ${sd}
-**End Date** ${ed}`)
-				.setFooter('Click on the title for full synopsis [You can use -2 to search for the next result]');
-			message.channel.send(aniEmbed);
-		}).catch((e) => {
-			if (e.name === 'null') {
-				console.log('ping');
-			}
-			console.error(e);
+					.setTitle(`${aniRes.title.romaji} [${aniRes.title.native}]`)
+					.setURL(aniRes.siteUrl)
+					.setThumbnail(aniRes.coverImage.large)
+					.setDescription(`**Status** ${status} | **Type** ${type} | **Episodes** ${episodes}
+**Studio** ${studio} | **Average Score** ${avgScore}%
+**Genres** ${genres}
+					
+**Start Date** ${startDate}
+**End Date** ${endDate}
+					
+**Description**
+${longDesc}`);
+				message.channel.send(aniEmbed);
+			});
 		});
 	},
 };
