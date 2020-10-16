cd7cff6995402146a7ba82df52ec7fb567646cbc
Brody Jagoe <68491774+Klarp@users.noreply.github.com>
2020-10-16 14:14:15 -0400

    Update fake.js
    - Added addtional png

--- commands/Owner/fake.js
+++ commands/Owner/fake.js
@@ -6,17 +6,30 @@ module.exports = {
 	description: 'Creates a fake halloween popup',
 	module: 'Owner',
 	owner: true,
-	execute(message) {
+	execute(message, args) {
         const osu_server = Client.guilds.cache.get('98226572468690944');
         const offtopic = osu_server.channels.cache.get('158481913055674368');
         const general = osu_server.channels.cache.get('98226572468690944');
         
-        const fakeEmbed = new MessageEmbed()
-            .setTitle('A trick-or-treater has stopped by!')
-            .setColor('#7289DA')
-            .setDescription('Open the door to greet them with h!treat')
-            .setImage('https://cdn.discordapp.com/halloween-bot/Man-shark.png');
+        const man_shark = 'https://cdn.discordapp.com/halloween-bot/Man-shark.png';
+        const centaur = 'https://cdn.discordapp.com/halloween-bot/Centaur.png';
         
-        general.send(fakeEmbed);
+        if (args === '1') {
+            const manSharkEmbed = new MessageEmbed()
+                .setTitle('A trick-or-treater has stopped by!')
+                .setColor('#7289DA')
+                .setDescription('Open the door to greet them with h!treat')
+                .setImage('https://cdn.discordapp.com/halloween-bot/Man-shark.png');
+        
+            general.send(fakeEmbed);
+        } else {
+            const manSharkEmbed = new MessageEmbed()
+                .setTitle('A trick-or-treater has stopped by!')
+                .setColor('#7289DA')
+                .setDescription('Open the door to greet them with h!treat')
+                .setImage('https://cdn.discordapp.com/halloween-bot/Man-shark.png');
+        
+            general.send(fakeEmbed);
+        }
 	},
 };
