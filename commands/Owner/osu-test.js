e9478770c40b27cc00d63e58cca08f8028c5d68d
Brody Jagoe <68491774+Klarp@users.noreply.github.com>
2020-11-12 13:12:52 -0500

    Rank Verification
    - Added leaderboard.js
    - Added mode.js
    - Updated osu-test.js to search user info
    - Added verify.js
    - Updated index.js
    - Updated Users.js
    - Updated package.json
    - Added getRankRole.js

--- commands/Owner/osu-test.js
+++ commands/Owner/osu-test.js
@@ -1,12 +1,41 @@
+const { MessageEmbed } = require('discord.js');
+const { Users } = require('../../dbObjects');
+
 module.exports = {
 	name: 'osu-test',
 	aliases: ['osutest', 'testosu'],
 	description: 'Tests osu! commands',
 	module: 'Owner',
 	owner: true,
-	args: true,
-	usage: '<beatmap> <acc> <combo> <missed> <mods>',
-	async execute() {
-		console.log('oink');
+	async execute(message, args) {
+		let member;
+		if (message.mentions.users.first()) {
+			member = message.mentions.members.first();
+		} else if (args[0]) {
+			member = message.guild.members.cache.get(args[0]);
+		} else {
+			member = message.member;
+		}
+
+		const user = await Users.findOne({ where: { user_id: member.id } });
+		if (!user) return message.reply('Could not find user!');
+
+		const mode = user.get('osu_mode');
+		const std_rank = user.get('std_rank');
+		const taiko_rank = user.get('taiko_rank');
+		const ctb_rank = user.get('ctb_rank');
+		const mania_rank = user.get('mania_rank');
+
+		const searchEmbed = new MessageEmbed()
+			.setAuthor(`${member.user.tag} (${member.id})`)
+			.setTitle(`${member.displayName}'s Info`)
+			.setColor('0xff69b4')
+			.setDescription(`Current Mode: ${mode}
+osu!std: ${std_rank}
+osu!taiko: ${taiko_rank}
+osu!ctb: ${ctb_rank}
+osu!mania: ${mania_rank}`);
+
+		message.channel.send(searchEmbed);
 	},
 };
--- models/Users.js
+++ models/Users.js
@@ -5,10 +5,39 @@ module.exports = (sequelize, DataTypes) => {
 			primaryKey: true,
 			unique: false,
 		},
-		user_osu: {
+		osu_name: {
 			type: DataTypes.STRING,
 			unique: false,
 		},
+		osu_id: {
+			type: DataTypes.STRING,
+			unique: false,
+		},
+		verified_id: {
+			type: DataTypes.STRING,
+			unique: false,
+		},
+		osu_mode: {
+			type: DataTypes.INTEGER,
+			unique: false,
+			defaultValue: 0,
+		},
+		std_rank: {
+			type: DataTypes.STRING,
+			unique: false,
+		},
+		taiko_rank: {
+			type: DataTypes.STRING,
+			unique: false,
+		},
+		mania_rank: {
+			type: DataTypes.STRING,
+			unique: false,
+		},
+		ctb_rank: {
+			type: DataTypes.STRING,
+			unique: false,
+		}
 	},	{
 		timestamps: false,
 	});
