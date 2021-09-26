977f902231562e933f5db1b4d328242a0900178b
Brody Jagoe <68491774+Klarp@users.noreply.github.com>
2021-09-26 15:22:31 -0600

    Update index.js
    - Fixed partial not being within the options of the client

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const { Muted, sConfig } = require('./dbObjects');
 // const { Users, Muted, sConfig } = require('./dbObjects');
 
 const configs = new Collection();
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] }, { partials: ['CHANNEL'] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES], partials: ['CHANNEL'] });
 client.commands = new Collection();
 const cooldowns = new Collection();
 exports.Client = client;
